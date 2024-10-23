81d3da482b8b7c07522dc5fcdb499a96dcf5c9e0
Ueli Haldemann <u.haldemann@unico.ch>
2024-10-23 09:42:14 +0200

    first

---
+++ Prog/testsend.bat
@@ -0,0 +1,3 @@
+"L:\Kunden\Betagtenzentren Emmen AG\Prog\smtpsend.exe" -fu.haldemann@unico.ch -hmail.aare-rz.ch -sPraesenzkennzahlen 2012 -tu.haldemann@unico.ch -iL:\Kunden\Betagtenzentren Emmen AG\Prog\Body.txt -aL:\Kunden\Betagtenzentren Emmen AG\Prog\2012rpt_KopfblattBZE.pdf
+
+pause
---
+++ testsenden.bat
@@ -0,0 +1,8 @@
+L:\KUNDEN\Griwa\KundenDB\NeueVersion\smtpsend.exe  -fu.haldemann@unico.ch -tu.haldemann@unico.ch -hmail.aare-rz.ch  -iL:\KUNDEN\Griwa\KundenDB\NeueVersion\test.txt -aohneAnhang
+
+
+
+
+
+
+Pause
